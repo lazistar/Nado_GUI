e9dfeffc85fbdadcf41f391688b6d8efc9820e2e
lazistar <hsjin1456@gmail.com>
2021-03-04 13:45:52 +0900

    revers or reversed
    revers와 reversed 명령어의 차이점에 대해 배웠다.

---
+++ gui_project/practice.py
@@ -0,0 +1,11 @@
+# lst = [1,2,3,4,5]
+# print(lst)
+# lst.reverse()
+# print(lst)
+
+lst2 = [1,2,3,4,5]
+print("리스트 2 뒤집기 전 : ", lst2)
+
+lst3 = reversed(lst2)
+print("리스트 2 뒤집은 후 : ", lst2)
+print("리스트 3 : ", list(lst3))
